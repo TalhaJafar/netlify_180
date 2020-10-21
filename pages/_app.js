031f53176ab89734341438fd3fd00c27f15cf4af
Talha Jafar <talhajafar37@outlook.com>
2020-10-21 18:28:19 +0500

    app changes

--- pages/_app.js
+++ pages/_app.js
@@ -18,41 +18,41 @@ const MyApp = (props) => {
   const [isValidCookie, setIsValidCookie] = useState(false);
   const vari = process.env.API_BASE;
 
-  // if (props.cook == null) {
-  //   return <AgeVerification route={props.router.route} />
-  // }
-  // else {
+  if (props.cook == null) {
+    return <AgeVerification route={props.router.route} />
+  }
+  else {
     return (
       <ApolloProvider client={client}>
         <Component {...pageProps} />
       </ApolloProvider>
     )
-  // }
+  }
 
 }
 
-// MyApp.getInitialProps = async (context) => {
-//   const { ctx } = context;
+MyApp.getInitialProps = async (context) => {
+  const { ctx } = context;
 
-//   const getCookie = (name, ctx) => {
-//     var nameEQ = name + '=';
-//     const cookies = ctx.req ? ctx.req.headers.cookie : document.cookie;
-//     if (!cookies) {
-//       return null;
-//     }
-//     var ca = cookies.split(';');
-//     for (var i = 0; i < ca.length; i++) {
-//       var c = ca[i];
-//       while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-//       if (c.indexOf(nameEQ) == 0)
-//         return c.substring(nameEQ.length, c.length);
-//     }
-//     return null;
-//   };
-//   const cookie = getCookie('age_verified', ctx);
-//   return {
-//     cook: cookie
-//   };
-// }
+  const getCookie = (name, ctx) => {
+    var nameEQ = name + '=';
+    const cookies = ctx.req ? ctx.req.headers.cookie : document.cookie;
+    if (!cookies) {
+      return null;
+    }
+    var ca = cookies.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0)
+        return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  };
+  const cookie = getCookie('age_verified', ctx);
+  return {
+    cook: cookie
+  };
+}
 
 export default MyApp
